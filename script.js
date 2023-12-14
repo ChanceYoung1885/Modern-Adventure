6808df5b8ba35e586d5a5659f58c0fa41ee0163e
Chance <chancemyoung@gmail.com>
2023-12-13 16:43:48 -0800

    Finished Anxiety and Time modifying functions. Tried to start working on options but got a little confused so nixed it.

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 function gameOver() {
     const clearTheScreen = document.getElementById('full-screen');
     clearTheScreen.innerHTML = 
-    "<div id='game-over'> <h1>Mass Text: </h1><h2>Sorry guys, I had an emergency come up and have to cancel tonight. Rain check?</h2><div>";
+    "<div id='game-over'> <h1>Mass Text: </h1><h2>Sorry guys, I had an emergency come up and have to cancel tonight. Rain check?</h2><button id='restart-button'>Try again?</button><div>";
 }
 
 //Function that changes the Anxiety Level and the color of its text to better represent the urgency and has a game over function
-function updateAnxietyLevel(newLevel) {
+function updateAnxietyLevel(anxietyMod) {
     const anxietyLevelElement = document.getElementById('anxiety-level');
+    let newLevel = parseInt(anxietyLevelElement.innerText) + anxietyMod;
     anxietyLevelElement.innerText = newLevel;
 
     if (newLevel <= 10) {
@@ -27,26 +28,25 @@ function updateAnxietyLevel(newLevel) {
 
 //Function that changes the Time Limit and has a game over function
 function updateTimeLimit(timeLoss) {
-    var hoursLeft = parseInt(document.getElementById('hours-left').innerText);
-    var minutesLeft = parseInt(document.getElementById('minutes-left').innerText);
+    var hoursLeftElement = document.getElementById('hours-left');
+    var minutesLeftElement = document.getElementById('minutes-left');
+    var hoursLeft = parseInt(hoursLeftElement.innerText);
+    var minutesLeft = parseInt(minutesLeftElement.innerText);
 
-    if (minutesLeft > timeLoss) {
-        minutesLeft -= timeLoss
-    } else if (hoursLeft > 0){
-        hoursLeft -= 1;
-        minutesLeft += 60;
-        if (minutesLeft > timeLoss) {
-            minutesLeft -= timeLoss
-        } else if (hoursLeft > 0){
-            hoursLeft -= 1;
-            minutesLeft += 60;
-            if (minutesLeft > timeLoss) {
-                minutesLeft -= timeLoss
-            } else {
-                gameOver;
-            }
-        }
+    //Combining hours and minutes into a total amount of minutes
+    var totalMinutesLeft = hoursLeft * 60 + minutesLeft;
+
+    //If the time loss can be subtracted it is, otherwise the game is over
+    if (totalMinutesLeft > timeLoss) {
+        totalMinutesLeft -= timeLoss;
+        //Separate hours and minutes
+        hoursLeft = Math.floor(totalMinutesLeft / 60);
+        minutesLeft = totalMinutesLeft % 60;
+        //Reassign new time values to their perspective elements
+        hoursLeftElement.innerText = hoursLeft;
+        minutesLeftElement.innerText = minutesLeft;
     } else {
-        gameOver;
+        gameOver();
     }
 }
+
